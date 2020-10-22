9bc12117448bb9a501a3f384f7605daa7f4bd0b6
Claud Watari <claudwatari95@gmail.com>
2020-10-22 23:35:10 +0300

    feat: init login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import{ ActivatedRoute, Router } from '@angular/router';
+
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @Input() userModel = {
+    email: '',
+    password: ''
+  };
+
+  someData: any;
+  
   isThisYear = new Date().getFullYear();
-  constructor() { }
+  constructor(private loginService: LoginService, private Route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  createUser() {
+    const checkerbtn = document.querySelector('.btn_p');
+    const alert_ = document.querySelector('#alert_') as HTMLDivElement;
+
+    checkerbtn.textContent = 'Checking. Please wait...';
+
+    this.loginService.loginUser(this.userModel).subscribe((res) => {
+
+      this.someData = res;
+
+      if(res !== undefined && typeof res === 'object') {
+
+        localStorage.setItem('f28authkey', this.someData.data.token);
+        checkerbtn.textContent = 'Redirecting...'
+        console.log(this.someData);
+
+      } else {
+
+        checkerbtn.textContent = 'Encountered an error!';
+
+        const errMsg = localStorage.getItem('f28err');
+
+        alert_.textContent = errMsg;
+        alert_.style.display = 'block';
+
+        const revertBtnText = () => {
+          checkerbtn.textContent = 'Create my account';
+          alert_.style.display = 'none';
+          localStorage.removeItem('f28err');
+        }
+
+        setTimeout(revertBtnText, 3000);
+      }
+    });
+
+
+  };
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, retry, tap, map } from 'rxjs/operators';
+import { globalUri } from '../env/env';
+
+const endpoint = globalUri.apiGlobal;
+
+const HttpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  };
+
+  private handleError<T>(operation = 'operation', result ?: T) {
+    return (error: any) : Observable<T> => {
+      console.error(error.error);
+      console.log(`${operation} failed: ${error.message}`);
+
+      localStorage.setItem('f28err', error.error.error);
+
+      return of(result as T);
+    }
+  };
+
+  loginUser(user) : Observable<any> {
+    return this.http.post<any>(`${endpoint}/auth/login-user`, JSON.stringify(user), HttpOptions).pipe(
+      map((_user) => {
+        _user.json();
+      }
+    ), catchError(this.handleError<any>('Log in'))
+    );
+  }
+};
