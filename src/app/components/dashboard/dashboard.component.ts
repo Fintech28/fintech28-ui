d6f84d43d24c59bb539c7926c48905a47f53c61c
Claud Watari <claudwatari95@gmail.com>
2020-10-23 13:43:54 +0300

    fix: init dashboard views # 01

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  userId:Number;
   constructor() { }
 
   ngOnInit(): void {
+    this.userId = 1;
   }
 
 }
