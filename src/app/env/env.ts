b0ff319f8042a52a4e38193557751c5c47d8cfeb
Claud Watari <claudwatari95@gmail.com>
2020-10-24 12:34:35 +0300

    feat: init dashboard service

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,3 +1,4 @@
 export const globalUri = {
-    apiGlobal: 'https://fintech28.herokuapp.com/api/v1'
+    apiGlobal: 'https://fintech28.herokuapp.com/api/v1',
+    token: localStorage.getItem('f28authkey')
 };
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, retry, tap, map } from 'rxjs/operators';
+import { globalUri } from '../env/env';
+
+const endpoint = globalUri.apiGlobal;
+
+const token = globalUri.token;
+
+const HttpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': token,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  getAuthData() : Observable<any> {
+    return this.http.get<any>(`${endpoint}/logged-data`, HttpOptions);
+  };
+};
